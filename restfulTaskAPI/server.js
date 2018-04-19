9cc84bcf65f98f31988c0ecd250e17ab31897910
Jack Hyc <Jacek.Hyc@gmail.com>
2018-04-19 09:26:51 -0500

    adding completed Hello Angular assignment

--- restfulTaskAPI/server.js
+++ restfulTaskAPI/server.js
@@ -3,6 +3,7 @@ var app = express();
 var path = require("path");
 var bodyParser = require('body-parser'); 
 
+app.use(express.static( __dirname + '/helloAngular/dist' ));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "./client/static")));
